d8a417fc69fab71834bb0fe7cd58f666a61af059
Márcio de Souza Júnior <marciosouzajunior@gmail.com>
2022-02-24 17:32:51 -0300

    Parâmetro das teclas.

--- MiniPiano/MiniPiano/MainPage.xaml.cs
+++ MiniPiano/MiniPiano/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Plugin.SimpleAudioPlayer;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -38,6 +39,15 @@ namespace MiniPiano
 
             // White keys
             int yPosition = 0;
+            Queue<string> whiteKeys = new Queue<string>(new string[]{
+                "C1", "D1", "E1", "F1", "G1", "A1", "B1",
+                "C2", "D2", "E2", "F2", "G2", "A2", "B2",
+                "C3", "D3", "E3", "F3", "G3", "A3", "B3",
+                "C4", "D4", "E4", "F4", "G4", "A4", "B4",
+                "C5", "D5", "E5", "F5", "G5", "A5", "B5",
+                "C6", "D6", "E6", "F6", "G6", "A6", "B6",
+                "C7", "D7", "E7", "F7", "G7", "A7", "B7"
+            });
 
             for (int i = 1; i <= 56; i++)
             {
@@ -51,7 +61,7 @@ namespace MiniPiano
 
                 TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
                 tapGestureRecognizer.Tapped += WhiteKeyTapped;
-                tapGestureRecognizer.CommandParameter = i;
+                tapGestureRecognizer.CommandParameter = whiteKeys.Dequeue();
                 whiteKey.GestureRecognizers.Add(tapGestureRecognizer);
 
                 pianoLayout.Children.Add(whiteKey);
@@ -61,6 +71,15 @@ namespace MiniPiano
             // Black keys
             yPosition = 75;
             int[] steps = new int[] { 3, 7, 10, 14, 17, 21, 24, 28, 31, 35, 38, 42, 45, 49, 52, 56 };
+            Queue<string> blackKeys = new Queue<string>(new string[]{
+                "Db1", "EB1", "Gb1", "Ab1", "Bb1",
+                "Db2", "EB2", "Gb2", "Ab2", "Bb2",
+                "Db3", "EB3", "Gb3", "Ab3", "Bb3",
+                "Db4", "EB4", "Gb4", "Ab4", "Bb4",
+                "Db5", "EB5", "Gb5", "Ab5", "Bb5",
+                "Db6", "EB6", "Gb6", "Ab6", "Bb6",
+                "Db7", "EB7", "Gb7", "Ab7", "Bb7"
+            });
 
             for (int i = 1; i <= 56; i++)
             {
@@ -83,7 +102,7 @@ namespace MiniPiano
 
                 TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
                 tapGestureRecognizer.Tapped += WhiteKeyTapped;
-                tapGestureRecognizer.CommandParameter = i;
+                tapGestureRecognizer.CommandParameter = blackKeys.Dequeue();
                 blackKey.GestureRecognizers.Add(tapGestureRecognizer);
 
                 pianoLayout.Children.Add(blackKey);
